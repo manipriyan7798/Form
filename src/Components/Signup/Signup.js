9e4a35b9298cc82a5d94712ce28d87f411a8f4a7
manipriyan7798 <manipriyan@6storage.com>
2022-02-22 00:50:26 +0530

    added some changes to validation

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -21,35 +21,54 @@ const Signup = () => {
   const [genderValidation, setGenderValidation] = useState(false);
   const [birthdayValidation, setbirthdayValidation] = useState(false);
 
-  const validate = () => {
+  const validate = (e) => {
     if (firstName === "" || firstName === undefined) {
       setFirstNameValidation(true);
+      e.preventDefault();
     }
     if (lastName === "" || lastName === undefined) {
       setLastNameValidation(true);
+      e.preventDefault();
     }
     if (email === "" || email === undefined) {
       setEmailValidation(true);
+      e.preventDefault();
     }
     if (gender === "" || gender === undefined) {
       setGenderValidation(true);
+      e.preventDefault();
     }
     if (birthday === "" || birthday === undefined) {
-      setbirthdayValidation(true)
+      setbirthdayValidation(true);
+      e.preventDefault();
     }
     if (password === "" || password === undefined) {
+      e.preventDefault();
       setPasswordValidation(true);
     }
     if (confirmPassword === "" || confirmPassword === undefined) {
+      e.preventDefault();
       setConfirmpasswordValidation(true);
     }
 
     if (!firstName || !lastName || !email || !genderValidation || !password || !confirmPassword) {
       return false;
+    } else {
+      e.preventDefault();
+      console.log({
+        firstName: firstName,
+        lastName: lastName,
+        date_of_Birth: birthday,
+        mail: email,
+        password: password,
+        gender: gender,
+      });
     }
 
     //if password & confirmPassword not equal
     if (password !== confirmPassword) {
+      e.preventDefault();
+
       alert("password and confirm password is not equal");
     }
 
@@ -62,9 +81,14 @@ const Signup = () => {
     //   alert("Please enter all the details");
     // } else {
     if (validate) {
-      console.log(
-        { firstName: firstName, lastName: lastName, date_of_Birth: birthday, mail: email, password: password, gender: gender }
-      );
+      console.log({
+        firstName: firstName,
+        lastName: lastName,
+        date_of_Birth: birthday,
+        mail: email,
+        password: password,
+        gender: gender,
+      });
     }
     // setFirstName("");
     // setLasttName("");
@@ -75,10 +99,7 @@ const Signup = () => {
     // } else {
     //   console.log("signup unsuccessfull");
     // }
-
-
   };
-
 
   const checkboxChangeHandler = (e) => {
     // to find out if it's checked or not; returns true or false
@@ -103,7 +124,7 @@ const Signup = () => {
   return (
     <div className="signup-container">
       <h1>Signup</h1>
-      <form onSubmit={submitButtonHandler} >
+      <form onSubmit={validate}>
         {/* first name field starts */}
         <Form.Group className="mb-3">
           <Form.Label>First Name:</Form.Label>
@@ -113,20 +134,19 @@ const Signup = () => {
             name="firstName"
             value={firstName}
             onChange={(e) => {
-              setFirstName(e.target.value)
-              setFirstNameValidation(false)
-            }} />
+              setFirstName(e.target.value);
+              setFirstNameValidation(false);
+            }}
+          />
           {/* name Validation */}
-          {firstNameValidation &&
-            <Form.Text className="text-danger">
-              First Name is Required
-            </Form.Text>
-          }
+          {firstNameValidation && (
+            <Form.Text className="text-danger">First Name is Required</Form.Text>
+          )}
         </Form.Group>
         {/* first name field ends */}
 
         {/* Last name field starts */}
-        <Form.Group className="mb-3" >
+        <Form.Group className="mb-3">
           <Form.Label>Last Name :</Form.Label>
           <Form.Control
             type="text"
@@ -134,28 +154,30 @@ const Signup = () => {
             name="lastName"
             value={lastName}
             onChange={(e) => {
-              setLasttName(e.target.value)
-              setLastNameValidation(false)
+              setLasttName(e.target.value);
+              setLastNameValidation(false);
             }}
             placeholder="Last Name"
           />
           {/* name Validation */}
-          {lastNameValidation &&
-            <Form.Text className="text-danger">
-              Last Name is Required
-            </Form.Text>
-          }
+          {lastNameValidation && (
+            <Form.Text className="text-danger">Last Name is Required</Form.Text>
+          )}
         </Form.Group>
         {/* Last name field ends */}
 
         {/* Date picker field starts */}
-        <Form.Group className="mb-3" >
+        <Form.Group className="mb-3">
           <Form.Label>Enter Date of birth:</Form.Label>
           <Form.Control
             type="date"
             name="birthday"
             value={birthday}
-            onChange={(e) => setBirthday(e.target.value)}
+            // onChange={(e) => setBirthday(e.target.value)}
+            onChange={(e) => {
+              setBirthday(e.target.value);
+              setbirthdayValidation(false);
+            }}
             placeholder="Birthday"
             maxLength={moment().format()}
           />
@@ -163,19 +185,17 @@ const Signup = () => {
 
           {/* {moment(birthday).format("ll")} */}
           {/* name Validation */}
-          {birthdayValidation &&
-            <Form.Text className="text-danger">
-              Date of birth is Required
-            </Form.Text>
-          }
+          {birthdayValidation && (
+            <Form.Text className="text-danger">Date of birth is Required</Form.Text>
+          )}
         </Form.Group>
         {/* Date picker field ends */}
-
 
         {/* gender radio button starts*/}
         <Form.Check
           inline
           label="Male"
+          id="Male"
           name="gender"
           value={gender}
           onChange={(e) => setGender(e.target.id)}
@@ -183,6 +203,7 @@ const Signup = () => {
         />
         <Form.Check
           inline
+          id="Female"
           label="Female"
           name="gender"
           value={gender}
@@ -190,16 +211,11 @@ const Signup = () => {
           type="radio"
         />
 
-        {
-          genderValidation &&
-          <Form.Text className="text-danger">
-            Gender is Required
-          </Form.Text>
-        }
+        {genderValidation && <Form.Text className="text-danger">Gender is Required</Form.Text>}
         {/* gender radio button ends */}
 
         {/* email validation starts */}
-        <Form.Group className="mb-3" >
+        <Form.Group className="mb-3">
           <Form.Label>Email :</Form.Label>
           <Form.Control
             type="text"
@@ -207,21 +223,17 @@ const Signup = () => {
             name="email"
             value={email}
             onChange={(e) => {
-              setEmail(e.target.value)
-              setEmailValidation(false)
+              setEmail(e.target.value);
+              setEmailValidation(false);
             }}
             placeholder="Email"
           />
           {/* name Validation */}
-          {emailValidation &&
-            <Form.Text className="text-danger">
-              Email ID is Required
-            </Form.Text>
-          }
+          {emailValidation && <Form.Text className="text-danger">Email ID is Required</Form.Text>}
         </Form.Group>
 
         {/* Password field starts */}
-        <Form.Group className="mb-3" >
+        <Form.Group className="mb-3">
           <Form.Label htmlFor="inputPassword5">Password</Form.Label>
           <Form.Control
             type="password"
@@ -233,19 +245,15 @@ const Signup = () => {
             minLength={6}
           />
           {/* passowrd validation */}
-          {
-            passwordValidation &&
-            <Form.Text className="text-danger">
-              Password is Required
-            </Form.Text>
-          }
+          {passwordValidation && (
+            <Form.Text className="text-danger">Password is Required</Form.Text>
+          )}
         </Form.Group>
         {/* Password filed ends */}
 
-
         {/* confirm password starts */}
-        <Form.Group className="mb-3" >
-          <Form.Label htmlFor="inputPassword5"  >Confirm Password</Form.Label>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="inputPassword5">Confirm Password</Form.Label>
           <Form.Control
             type="password"
             name="password"
@@ -256,12 +264,9 @@ const Signup = () => {
             minLength={6}
           />
           {/* passowrd validation */}
-          {
-            confirmpasswordValidation &&
-            <Form.Text className="text-danger">
-              Confirm Password is Required
-            </Form.Text>
-          }
+          {confirmpasswordValidation && (
+            <Form.Text className="text-danger">Confirm Password is Required</Form.Text>
+          )}
         </Form.Group>
         {/* confirm password ends */}
 
